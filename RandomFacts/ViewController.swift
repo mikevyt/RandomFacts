983945b9181c5c853459344cefb7fa299f052724
Michael Vytlingam <michael.vytlingam@gmail.com>
2018-12-08 18:49:29 -0500

    Initial Commit

---
+++ RandomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFacts
+//
+//  Created by Michael Vytlingam on 2018-12-08.
+//  Copyright © 2018 Michael Vytlingam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
